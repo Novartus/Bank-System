4ea401ff34ac24bc2250ec391d835e2884f37ea5
Abhee Hudani <hudaniabhee@gmail.com>
2020-03-22 19:28:33 +0530

    Obj/Debug Updated

--- obj/Debug/The-Simple-Bank_Content.g.i.cs
+++ obj/Debug/The-Simple-Bank_Content.g.i.cs
@@ -8,6 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("warning.jpg")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("abhee-1.jpeg")]
 
 
